f6a3f813d633ff628de2cb04e6d37c98296212b1
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2021-10-07 19:49:10 +0300

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
     grunt.registerTask("upload", async function () {
         grunt.log.ok("Modules uploaded");
+        console.log(process.env)
+        grunt.log.ok(process.env)
     });
 };
