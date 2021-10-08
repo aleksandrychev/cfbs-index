d673aed70bb08c6503a426987aba94cfa93a3837
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2021-10-08 10:44:03 +0300

    f

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,20 @@
 module.exports = function (grunt) {
+
+    grunt.registerTask("versioning", async function () {
+        grunt.log.ok("Modules uploaded");
+        const workdir = process.env.GITHUB_WORKSPACE;
+        const index = grunt.file.readJSON(`${workdir}/index.json`);
+        if (!index.hasOwnProperty('modules')) {
+            grunt.log.error("index.json misses modules");
+        }
+    });
+
     grunt.registerTask("upload", async function () {
         grunt.log.ok("Modules uploaded");
-        console.log(process.env)
-        grunt.log.ok(process.env)
+        const workdir = process.env.GITHUB_WORKSPACE;
+        const index = grunt.file.readJSON(`${workdir}/index.json`);
+        if (!index.hasOwnProperty('modules')) {
+            grunt.log.error("index.json misses modules");
+        }
     });
 };
