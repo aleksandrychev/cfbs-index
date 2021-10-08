1660d85bcfbc82ac1411f75eccb98760239b7c00
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2021-10-08 20:00:02 +0300

    upd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,3 @@
-const shell = require("shelljs");
-const path = require("path");
-const md5File = require("md5-file");
 module.exports = function (grunt) {
     const workdir = process.env.GITHUB_WORKSPACE;
     const index = grunt.file.readJSON(`${workdir}/index.json`);
@@ -27,14 +24,29 @@ module.exports = function (grunt) {
         }
     });
 
+
+
     const shell = require('shelljs');
     const path = require('path');
-    const md5File = require('md5-file')
+    const crypto = require('crypto')
+    const fs = require('fs')
+
+    const createHashFromFile = filePath => new Promise(resolve => {
+        const hash = crypto.createHash('sha256');
+        fs.createReadStream(filePath).on('data', data => hash.update(data)).on('end', () => resolve(hash.digest('hex')));
+    });
+
     grunt.registerTask("upload", async function () {
+        const done = this.async();
         const checksums = grunt.file.readJSON(`${workdir}/checksums.json`);
+        const readmeRegex = new RegExp('(readme\.md|readme\.org)', 'i');
+
         const tmp = shell.pwd().toString() + '/tmp';
+        shell.rm('-rf', tmp);
         shell.mkdir(tmp);
-        Object.keys(index.modules).forEach(key => {
+
+
+        for (const key of Object.keys(index.modules)) {
             const module = index.modules[key];
             if (!module.hasOwnProperty('alias')) {
                 if (!checksums.hasOwnProperty(key)) {
@@ -42,7 +54,6 @@ module.exports = function (grunt) {
                 }
                 // if no checksum then upload to S3 and create checksum
                 if (!checksums[key].hasOwnProperty(module.commit)) {
-                    // if (key != 'autorun') return;
                     shell.cd(tmp);
                     shell.exec(`git clone ${module.repo}`)
                     const moduleDir = path.basename(module.repo) + '/' + (module.subdirectory || '');
@@ -50,28 +61,31 @@ module.exports = function (grunt) {
                     shell.exec(`git checkout ${module.commit}`)
 
                     shell.ls('*.{md,org}').forEach(function (file) {
-                        // console.log(file)
+                        if (readmeRegex.test(file)) {
+                            console.log(file, 222)
+                        }
                     });
 
                     try {
-                        shell.exec(`tar -czvf ${module.commit}.tar.gz ${shell.pwd().toString()}`);
-                        const hash = md5File.sync(`${module.commit}.tar.gz`)
-                        console.log(`The MD5 sum of ${module.commit}.tar.gz is: ${hash}`)
+                        shell.exec(`tar --exclude .git -czvf ${module.commit}.tar.gz ./`);
+                        let hash = await createHashFromFile(`./${module.commit}.tar.gz`);
+                        console.log(`The sha256 sum of ${module.commit}.tar.gz is: ${hash}`)
                         checksums[key][module.commit] = hash;
-                        grunt.file.write(`${workdir}/checksums.json`, JSON.stringify(checksums, null, 2));
                     } catch (e) {
                         console.log(e)
                     }
 
-                    return;
-                    // download
-                    // calc checksum
+
+                    // + download
+                    // + calc checksum
                     // upload to S3
-                    // set check sum
-                    checksums[key][module.commit] = '';
+                    // + set check sum
+                    // console.log(checksums)
                 }
             }
-        })
-        grunt.file.write(`${workdir}/versions.json`, JSON.stringify(versions, null, 2));
+        }
+
+        grunt.file.write(`${workdir}/checksums.json`, JSON.stringify(checksums, null, 2));
+        done();
     });
 };
