4ba74e1c7c43c332c05814f58ef07d5936d9bdbb
Ihor Aleksandrychiev <ihor.aleksandrychiev@northern.tech>
2021-10-12 15:23:50 +0300

    update

--- scripts/s3uploading.js
+++ scripts/s3uploading.js
@@ -8,6 +8,7 @@ const workdir = process.env.GITHUB_WORKSPACE;
 const s3 = new AWS.S3();
 const tmp = shell.pwd().toString() + '/tmp';
 const readmeRegex = new RegExp('(readme\.md|readme\.org)', 'i');
+const Bucket = 'cfbs'
 
 const readJSON = (file) => JSON.parse(fs.readFileSync(file, 'utf8'));
 
@@ -17,7 +18,7 @@ const versions = readJSON(`${workdir}/versions.json`);
 let commitMsg = ["Updated versions.json \n"];
 
 const uploadFile = (file, s3path) => s3.upload({
-    Bucket: 'cfbs',
+    Bucket,
     Key: s3path,
     Body: fs.readFileSync(file),
     ACL: 'public-read'
@@ -40,6 +41,14 @@ const checkout = (module) => {
     console.log(module.commit)
 }
 
+const s3ObjExists = s3Path => new Promise(resolve => {
+    s3.headObject({Bucket, Key: s3Path}).on('success', function(response) {
+        resolve(true);
+    }).on('error',function(error){
+        resolve(false);
+    }).send();
+});
+
 const createHashFromFile = filePath => new Promise(resolve => {
     const hash = crypto.createHash('sha256');
     fs.createReadStream(filePath).on('data', data => hash.update(data)).on('end', () => resolve(hash.digest('hex')));
@@ -55,11 +64,33 @@ const createArchive = async (index, version, commit) => {
     }
 }
 
+const processReadme = async (moduleIndex, module) => {
+    let readme_url, readme_sha256 = null;
+    for (const file of shell.ls('*.{md,org}')) {
+        if (readmeRegex.test(file)) {
+            let s3ReadmePath = `modules/${moduleIndex}/${module.commit}${path.extname(file)}`;
+            uploadFile(file, `modules/${moduleIndex}/${module.commit}${path.extname(file)}`);
+            readme_url = s3ReadmePath;
+            readme_sha256 = await createHashFromFile(`./${file}`);
+        }
+    }
+    return {readme_url, readme_hash_sha256};
+}
+
 const processModules = async () => {
     for (const moduleIndex in modules) {
         const module = modules[moduleIndex];
+        const archiveS3Path = `modules/${moduleIndex}/${module.commit}.tar.gz`;
+
         if (moduleIndex != 'autorun') continue;
         if (module.hasOwnProperty('alias') || (versions.hasOwnProperty(moduleIndex) && versions[moduleIndex].hasOwnProperty(module.version))) continue;
+
+        const archiveExists = await s3ObjExists(archiveS3Path);
+
+        if (archiveExists) {
+            // console.error(`Skipped ${moduleIndex} ${module.version} version uploading to S3. Archive already exists: ${archiveS3Path}.`);
+            // return;
+        }
 
         shell.cd(tmp);
         checkout(module);
@@ -68,29 +99,23 @@ const processModules = async () => {
             versions[moduleIndex] = {};
         }
 
-        shell.ls('*.{md,org}').forEach(function (file) {
-            if (readmeRegex.test(file)) {
-                let s3ReadmePath = `modules/${moduleIndex}/${module.commit}${path.extname(file)}`;
-                uploadFile(file, `modules/${moduleIndex}/${module.commit}${path.extname(file)}`);
-                versions[moduleIndex][module.version] = {readme: s3ReadmePath};
-            }
-        });
-
+        const readme = await processReadme(moduleIndex, module);
         const archive = await createArchive(moduleIndex, module.version, module.commit);
 
-        uploadFile('./' + archive.path, `modules/${moduleIndex}/${archive.path}`);
+        uploadFile('./' + archive.path, `modules/${moduleIndex}/${module.commit}.tar.gz`);
 
         versions[moduleIndex][module.version] = {
-            "hash_sha256": archive.hash,
+            "archive_sha256": archive.hash,
             "commit": module.commit,
-            "archive_url": `/${moduleIndex}/${module.commit}.tar.gz`
+            "archive_url": `/${moduleIndex}/${module.commit}.tar.gz`,
+            ...readme
         };
         commitMsg.push(`- Added ${moduleIndex} ${module.version} version`)
     }
 }
 
 processModules().then(() => {
-    fs.writeFile(`${workdir}/versions.json`, JSON.stringify(versions, null, 2), function (err) {
+    fs.writeFile(`${workdir}/versions.json`, JSON.stringify(versions, null, 2) + "\n", function (err) {
         if (err) return console.error(err);
     });
 
